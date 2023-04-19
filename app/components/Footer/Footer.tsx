89ae7773e7c0459cd7d8c810cb5efda61c7d9fa4
YaredBG <Yared_Gari@Mckinsey.com>
2023-04-19 16:59:04 -0700

    added footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,12 +1,82 @@
-import React from 'react'
-import styles from "./Footer.module.css"
+import React from "react";
+import styles from "./Footer.module.css";
+import Image from "next/image";
+import foot1 from "../../../public/images/worldimg.avif";
+import { AllLinks } from "../../../public/Alltext";
+import {
+  EmailIcon,
+  PhoneIcon,
+  LocationIcon,
+  LinkedinIcon,
+  Github,
+} from "../../components/Icons/IconComponents/Icons";
+import Link from "next/link";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footerContainer}>
+      <div className={styles.innerContainer}>
+        <div className={styles.footerTextContainer}>
+          <div className={styles.content}>
+            <div>
+              <p className={styles.contactme}>CONTACT</p>
+              <ul className={styles.contactmeleft}>
+                <li className={styles.infocontainer}>
+                  <EmailIcon className={styles.iconcomponent} />
+                  <span className={styles.iconText}>
+                    <Link href={`mailto:${AllLinks.email1}`} target="_blank">
+                      Email
+                    </Link>
+                  </span>
+                </li>
+                <li className={styles.infocontainer}>
+                  <PhoneIcon className={styles.iconcomponent} />
+                  <span className={styles.iconText}>{AllLinks.phone}</span>
+                </li>
+                <li className={styles.infocontainer}>
+                  <LocationIcon className={styles.iconcomponent} />
+                  <span className={styles.iconText}>{AllLinks.location}</span>
+                </li>
+                <li className={styles.infocontainer}>
+                  <LinkedinIcon className={styles.iconcomponent} />
+                  <span className={styles.iconText}>
+                    <Link href={AllLinks.Linkedin} target="_blank">
+                      Linkedin
+                    </Link>
+                  </span>
+                </li>
+                <li className={styles.infocontainer}>
+                  <Github className={styles.iconcomponent} />
+                  <span className={styles.iconText}>
+                    <Link href={AllLinks.github} target="_blank">
+                      GitHub
+                    </Link>
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.divider}></div>
 
-export default Footer
+            <div className={styles.contactme}>
+              <div className={styles.social}>
+                <p>SOCIAL MEDIA</p>
+                <ul>
+                  <li className={styles.infocontainer}>twitter</li>
+                  <li className={styles.infocontainer}>Instagram</li>
+                  <li className={styles.infocontainer}>faceebook</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className={styles.copyright}>
+            &copy;2023 Designed and Implemented by Yared Gari
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/components/Icons/IconComponents/Icons.tsx
+++ app/components/Icons/IconComponents/Icons.tsx
@@ -835,3 +835,74 @@ export const TeamWork = ({ className = "", ...rest }) => (
     />
   </motion.svg>
 );
+
+export const EmailIcon = ({ className = "", ...rest }) => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="2rem"
+    height="2rem"
+    viewBox="0 0 24 24"
+    className={className}
+    {...rest}
+    whileHover={{ scale: 1.2 }}
+  >
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path
+      fill="#efefef"
+      d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"
+    />
+  </motion.svg>
+);
+export const PhoneIcon = ({ className = "", ...rest }) => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="2rem"
+    height="2rem"
+    viewBox="0 0 24 24"
+    className={className}
+    {...rest}
+    whileHover={{ scale: 1.2 }}
+  >
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path
+      fill="#efefef"
+      d="M6.54 5c.06.89.21 1.76.45 2.59l-1.2 1.2c-.41-1.2-.67-2.47-.76-3.79h1.51m9.86 12.02c.85.24 1.72.39 2.6.45v1.49c-1.32-.09-2.59-.35-3.8-.75l1.2-1.19M7.5 3H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.49c0-.55-.45-1-1-1-1.24 0-2.45-.2-3.57-.57a.84.84 0 0 0-.31-.05c-.26 0-.51.1-.71.29l-2.2 2.2a15.149 15.149 0 0 1-6.59-6.59l2.2-2.2c.28-.28.36-.67.25-1.02A11.36 11.36 0 0 1 8.5 4c0-.55-.45-1-1-1z"
+    />
+  </motion.svg>
+);
+
+export const LocationIcon = ({ className = "", ...rest }) => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="2rem"
+    height="2rem"
+    viewBox="0 0 24 24"
+    className={className}
+    {...rest}
+    whileHover={{ scale: 1.2 }}
+  >
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path
+      fill="#efefef"
+      d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zM7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 2.88-2.88 7.19-5 9.88C9.92 16.21 7 11.85 7 9z"
+    />
+    <circle cx={12} cy={9} r={2.5} />
+  </motion.svg>
+);
+
+export const LinkedinIcon = ({ className = "", ...rest }) => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    className={className}
+    {...rest}
+    whileHover={{ scale: 1.2 }}
+  >
+    <path
+      fill="#efefef"
+      d="M6.94 5a2 2 0 1 1-4-.002 2 2 0 0 1 4 .002ZM7 8.48H3V21h4V8.48Zm6.32 0H9.34V21h3.94v-6.57c0-3.66 4.77-4 4.77 0V21H22v-7.93c0-6.17-7.06-5.94-8.72-2.91l.04-1.68Z"
+    />
+  </motion.svg>
+);
